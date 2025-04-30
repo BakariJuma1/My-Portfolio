25435767defe880d40e14275df6fb789770678ef
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-01 01:49:27 +0300

    fixing my animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import Navbar from "./ui/Navbar";
 import MobileMenu from "./ui/MobileMenu";
+import LoadingScreenAnimation from "./ui/LoadingScreenAnimation";
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className="text-3xl font-bold">
+    <>
+      <LoadingScreenAnimation />
       <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-    </div>
+    </>
   );
 }
 
